46f96b74f82361c2a3ff7a655d36b4143e9ea6aa
JangJeaHun <wkdwogns2345@naver.com>
2022-05-12 15:32:55 +0900

    animation add

--- script/script.js
+++ script/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+  // Scroll Animation (sa, 스크롤 애니메이션)
+  const saTriggerMargin = 300;
+  const saElementList = document.querySelectorAll('.sa');
+
+  const saFunc = function() {
+    for (const element of saElementList) {
+      if (!element.classList.contains('show')) {
+        if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+          element.classList.add('show');
+        }
+      }
+    }
+  }
+
+  window.addEventListener('load', saFunc);
+  window.addEventListener('scroll', saFunc);
+
   
   var wid = $(window).width();
   console.log(wid)
